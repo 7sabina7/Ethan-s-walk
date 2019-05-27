5b95186b92d8e411bae94ca2beee912b09aaa1ee
7sabina7 <7sabina7@mail.ru>
2019-05-27 20:14:48 +0300

    Share project^C

---
+++ For Git/Assets/Scripts/DestroyTimeout.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class DestroyTimeout : MonoBehaviour
+{
+     public float timer = 6.0f;
+    void Start()
+    {
+        Destroy(gameObject, timer);
+
+    }
+}
